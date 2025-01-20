516485f5dbcae5fd64748fe55a40c6d21d772066
AngellaRod <intenserichroses@gmail.com>
2025-01-19 23:07:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ name = input('what is your name dear cook')
 age = input('Hello + name + how old are you cook?')
 Age_groups = ['[A] = 13-17','[B] = 18-30','[C] = 31+']
 print(Age_groups)
-choice = input('\nWhat is your choice? ')
-if choice == 'A':
+choice = input('\nWhat is your choice?')
+if choice == ['A']
     print('Cool but you should probably get your parents help too')
-else choice == 'B':
- print('Nice lets do it ')
-elif choice == 'C'
+else choice == ['B']
+ print('Nice lets do it')
+elif choice == ['C']
 
 def menu()
-print(' [Choice [1] What are the best methods to cook meat?')
-print(' [Choice [2] Which herb tastes well dry?')
-print(' [Choice [3] Nevermind but thank you')
+print('[Choice [1] What are the best methods to cook meat?')
+print('[Choice [2] Which herb tastes well dry?')
+print('[Choice [3] Nevermind but thank you')
 menu()
 Choice = int=input('What Choice will you pick dear cook')
-if Choice == 1
-print('The best message to cook mea t or high heat options, for example, grilling and broiling')
-else Choice == 2
+if Choice == [1]
+print('The best message to cook meat or high heat options, for example, grilling and broiling')
+else Choice == [2]
 print('herbs that taste well dry are parsley, rosemary, or oregano')
-elif Choice == 3
+elif Choice == [3]
 print('thank you for your time bye-bye')
